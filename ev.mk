d76c944e49a4cb93f3fcd03946e73c5106f17201
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-07-16 18:22:27 -0500

    passion: p2
    Change-Id: Ib72e0fff67ab0dbf5617009a8acd3f59d826d059

--- ev.mk
+++ ev.mk
@@ -25,9 +25,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_FINGERPRINT=google/pa
 #
 PRODUCT_CODENAME := Perdo
 ifeq ($(MINISKIRT),true)
-PRODUCT_VERSION_DEVICE_SPECIFIC := p1-StockHBoot
+PRODUCT_VERSION_DEVICE_SPECIFIC := p2-StockHBoot
 else
-PRODUCT_VERSION_DEVICE_SPECIFIC := p1
+PRODUCT_VERSION_DEVICE_SPECIFIC := p2
 endif
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Google Nexus One\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
