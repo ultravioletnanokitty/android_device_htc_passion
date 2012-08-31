a3f3a2edf2890eaa2b068416a04171de39977280
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-08-31 17:38:12 -0500

    passion: remove htc libril hack
    seems this is causing network detection errors
    
    Change-Id: I2bc643a56f4be0a279ee51ce4dd255207cf6e04e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -44,9 +44,6 @@ TARGET_PREBUILT_KERNEL  := device/htc/passion/prebuilt/kernel
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := mahimahi
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 3200
 
-# RIL
-BOARD_USE_NEW_LIBRIL_HTC := true
-
 # Hacks
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun0/file
 BOARD_USE_LEGACY_TRACKPAD       := true
