cbee66563a7e97ce84a92a0eb7d52fa572d959d6
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-07-29 23:18:03 -0500

    passion: cleanup
    * remove device version (setting at build time now)
    * remove intl dictionaries (smaller build)
    
    Change-Id: Ia3c53f29fb5eb2ad336b3d2a94dc38bd39e8a5ae

--- ev.mk
+++ ev.mk
@@ -20,16 +20,12 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_FINGERPRINT=google/pa
 # Set up the product codename, build version & MOTD.
 #
 PRODUCT_CODENAME := Perdo
-PRODUCT_VERSION_DEVICE_SPECIFIC := p1
 
 PRODUCT_MOTD :="\n\n\n--------------------MESSAGE---------------------\nThank you for choosing Evervolv for your Google Nexus One\nPlease visit us at \#evervolv on irc.freenode.net\nFollow @preludedrew for the latest Evervolv updates\nGet the latest rom at evervolv.com\n------------------------------------------------\n"
 
 # Copy compatible bootanimation
 PRODUCT_COPY_FILES += \
     vendor/ev/prebuilt/wvga/media/bootanimation.zip:system/media/bootanimation.zip
-
-# Pull all dictionaries
-TARGET_USE_KEYBOARD := international
 
 # Hot reboot
 PRODUCT_PACKAGE_OVERLAYS += vendor/ev/overlay/hot_reboot
